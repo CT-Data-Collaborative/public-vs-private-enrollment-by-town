e12cb040ffdd0a281531f88d20da57c8a27070ee
ilyankou <ilyankou@gmail.com>
2019-01-24 10:17:23 -0500

    Update with ACS2017 5y estimates

--- scripts/Public-vs-Private-Enrollment-by-Town-processing.R
+++ scripts/Public-vs-Private-Enrollment-by-Town-processing.R
@@ -17,7 +17,7 @@ source('./scripts/acsHelpers.R')
 
 #Get state data
 geography=geo.make(state=09)
-yearlist=c(2010:2016)
+yearlist=c(2010:2017)
 span = 5
 col.names="pretty" 
 key="ed0e58d2538fb239f51e01643745e83f380582d7"
@@ -33,8 +33,9 @@ for (i in seq_along(yearlist)) {
    k=k+1
   }  
   variable <- as.character(variable)    
-  data <- acs.fetch(geography=geography, endyear=endyear, span=span, 
-                    variable = variable, key=key)
+  data <- try(acs.fetch(geography=geography, endyear=endyear, span=span, 
+                    variable = variable, key=key))
+  print(data)
   year <- data@endyear
   print(paste("Processing: ", year))
   year <- paste(year-4, year, sep="-")
@@ -489,7 +490,7 @@ dataset <- dataset %>%
 
 write.table(
     dataset,
-    file.path("data", "public-vs-private-enrollment-town-2016.csv"),
+    file.path("data", "public-vs-private-enrollment-town-2017.csv"),
     sep = ",",
     row.names = F,
     na = "-9999"
